a73abc96d7093122f91e7e297bd03df72835bb3e
Mauro Rainis <mauro.rainis@gmail.com>
2019-09-15 07:08:29 +0200

    All the magic !

---
+++ MagiaEF/MagicContext.cs
@@ -0,0 +1,41 @@
+﻿using MagiaEF;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace MagiaEF
+{
+    class MagicContext : DbContext
+    {
+        private static string _connectionString;
+        public MagicContext() : base()
+        {
+
+        }
+        public DbSet<Sport> Sport { get; set; }
+        public DbSet<Studente> Studenti { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (string.IsNullOrEmpty(_connectionString))
+            {
+                LoadConnectionString();
+            }
+
+            optionsBuilder.UseSqlServer(_connectionString);
+        }
+
+        private static void LoadConnectionString()
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false)
+                ;
+
+            var configuration = builder.Build();
+            _connectionString = configuration.GetConnectionString("DefaultConnection");
+        }
+    }
+}
+
+    
--- MagiaEF/Program.cs
+++ MagiaEF/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace MagiaEF
 {
@@ -6,7 +7,25 @@ namespace MagiaEF
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            using (var ctx = new MagicContext())
+            {
+                var sport = ctx.Sport.ToList();
+                Console.WriteLine("Elenco sport memorizzati nel db");
+                foreach (var s in sport)
+                {
+                    Console.WriteLine(s.Nome);
+                }
+
+                Console.WriteLine();
+
+                var studenti = ctx.Studenti.ToList();
+                Console.WriteLine("Elenco studenti con relativo sport praticato");
+                foreach (var s in studenti)
+                {
+                    Console.WriteLine(s.Nome + " pratica " + (s.SportPraticato?.Nome ?? " nessuno sport") );
+                }
+
+            }
         }
     }
 }
