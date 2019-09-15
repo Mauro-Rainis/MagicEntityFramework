51d0063dc5c29668054bcbf9d6b862724e4ec388
Mauro Rainis <mauro.rainis@gmail.com>
2019-09-15 07:07:25 +0200

    Aggiunto script di creazone db e dati di prova

---
+++ MagiaEF/Sql/database.sql
@@ -0,0 +1,16 @@
+﻿create table sport(
+	id int, 
+	nome varchar(32)
+	);
+insert into sport values (1,'Mtb');
+insert into sport values (2,'Sci');
+insert into sport values (4,'Danza');
+
+create table studenti(
+	id int primary key, 
+	nome varchar(32), 
+	sportpraticatoid int
+	);
+insert into studenti values (1,'Mauro',1);
+insert into studenti values (2,'Luca',2);
+insert into studenti values (3,'Anna',NULL);
