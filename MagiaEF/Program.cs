5bbcf7109c8f1b1b3d749f3f2d968e109e9fb708
Mauro Rainis <mauro.rainis@gmail.com>
2019-09-15 06:47:56 +0200

    Add project files.

---
+++ MagiaEF/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MagiaEF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ MagiaEF/Studente.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MagiaEF
+{
+    class Studente
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public Sport SportPraticato { get; set; }
+    }
+}
