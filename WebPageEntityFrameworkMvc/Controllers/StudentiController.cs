a7ffbff50ae9046fffa369bb37095090f2a9cb46
Mauro Rainis <mauro.rainis@gmail.com>
2019-09-22 21:37:34 +0200

    Una possibile soluzione di Esercizio1

---
+++ WebPageEntityFrameworkMvc/Controllers/StudentiController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebPageEntityFrameworkMvc.Data;
+
+namespace WebPageEntityFrameworkMvc.Controllers
+{
+    public class StudentiController : Controller
+    {
+        private readonly MagicContext _context;
+
+        public StudentiController(MagicContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View(_context.Studenti.Include(s => s.SportPraticato));
+        }
+    }
+}
